420854cc179091cf15ef3d28c8a5c4a1bd7ac512
htorrence <hanna.torrence@gmail.com>
2015-03-08 20:19:44 -0500

    addition of adjacency list rep and more flexibility added to degree distribution code

---
+++ src/com/democracyapps/cnp/graphanalyzer/graph/AdjList.java
@@ -0,0 +1,7 @@
+package com.democracyapps.cnp.graphanalyzer.graph;
+
+/**
+ * Created by Hanna on 3/8/15.
+ */
+public class AdjList {
+}
---
+++ src/com/democracyapps/cnp/graphanalyzer/tasks/DegreeComparator.java
@@ -0,0 +1,7 @@
+package com.democracyapps.cnp.graphanalyzer.tasks;
+
+/**
+ * Created by Hanna on 3/8/15.
+ */
+public class DegreeComparator {
+}
